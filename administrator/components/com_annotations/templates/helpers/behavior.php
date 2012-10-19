2ba2c35befa2250a8a0fc7441a56803d3354c03b
Steven Rombauts <steven@timble.net>
2012-10-20 01:46:49 +0200

    - Finished 12.3 update - Use element's text (or title/src for images) as annotation title - Updated readme

---
+++ administrator/components/com_annotations/templates/helpers/behavior.php
@@ -0,0 +1,58 @@
+<?php
+class ComAnnotationsTemplateHelperBehavior extends KTemplateHelperBehavior
+{
+    public function annotations($config = array())
+    {
+    	if(strstr(KRequest::url(), '/administrator') === false) {
+    		return;
+    	}
+    	
+    	if(isset(self::$_loaded['annotations'])) {
+    		return;
+    	}
+    	
+    	$config = new KConfig($config);
+    	
+    	$view = $this->getTemplate()->getView();
+    	$layout = $view->getLayout();
+    	$identifier = ((string) $view->getIdentifier()) . '.' . $layout;
+    	
+    	$config->append(array(
+    			'mode' 			=> (JFactory::getUser()->gid == 25 ? 'manage' : 'view'),
+    			'identifier'  	=> $identifier
+    	));
+		
+		$this->mootools();
+		
+		$baseUrl = KRequest::base().'/';
+		$token = $this->getService('application.session')->getToken();
+		
+		$html = <<<END
+		<script src="media://com_annotations/js/annotation.js" />
+		<style src="media://com_annotations/css/annotation.css" />
+		<script>
+		var Annotations = Annotations || {};
+		
+		Annotations._mode = '{$config->mode}';
+		Annotations._baseUrl = '{$baseUrl}';
+		Annotations._token = '{$token}';
+		Annotations._identifier = '{$identifier}';
+		
+		window.addEvent('domready', function() {
+		
+			if(Annotations._mode == 'view')
+			{
+				$('command-help').addEvent('click', function(e) { 
+					e.stop();
+					Annotations.assistent.toggle.bind(Annotations.assistent)(); 
+	   			});
+	   		}
+		});
+		</script>
+END;
+		
+		self::$_loaded['annotations'] = true;
+		
+		return $html;
+    }
+}
